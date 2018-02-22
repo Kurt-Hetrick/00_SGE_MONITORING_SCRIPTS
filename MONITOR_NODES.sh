74fdfdc3b072f27ef79c3920e31c4e2662929183
Kurt Hetrick <khetric1@jhmi.edu>
2018-02-22 11:17:59 -0500

    MONITOR_NODES.sh now uses a refresh variable, also looks at all queues now. monitor.bright.cluster.sh also has a refresh variable now. just_grab_my_bad_jobs.sh is for pulling out the last X number jobs of a specific user (or all users) that had a non-zero exit or failure status.

--- MONITOR_NODES.sh
+++ MONITOR_NODES.sh
@@ -2,9 +2,11 @@
 
 module load sge
 
-while sleep 10 ;\
+REFRESH=$1
+
+while sleep $REFRESH ;\
  do clear;\
- qstat -f -q prod.q,test.q,rnd.q,cgc.q,bigmem.q,bigdata.q -s r \
+ qstat -f -s r \
 | egrep -v "^-|^[0-9]|^ " \
 | sed -r 's/[[:space:]]+/\t/g' \
 | awk 'BEGIN {FS="\t"};{OFS="\t"} {print $1,$3,$4,$6}' ;\
--- monitor.bright.cluster.sh
+++ monitor.bright.cluster.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+REFRESH=$1
+
 module load sge
 
-watch -n 30 'qhost | egrep -v "^-|global|node"'
+watch -n $REFRESH 'qhost | egrep -v "^-|global|node"'
