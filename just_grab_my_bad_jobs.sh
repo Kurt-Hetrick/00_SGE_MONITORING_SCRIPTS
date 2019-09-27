77588278f50633cde3c5baac9c960978222cd70a
Kurt Hetrick <khetric1@jhmi.edu>
2019-09-27 15:01:09 -0400

    for failed job trackers, fix bug so that it sorts only on job number

--- just_grab_my_bad_jobs.sh
+++ just_grab_my_bad_jobs.sh
@@ -7,6 +7,6 @@ awk 'BEGIN {FS=":"} $12!=0||$13!=0 {print $4,$2,$5,$6,strftime("%F.%H-%M-%S",$9)
 /cm/shared/apps/sge/var/default/common/accounting \
 | grep $USER \
 | tail -n $ROWS \
-| sort -k 4nr \
+| sort -k 4,4nr \
 | awk 'BEGIN {print "User","Host","Job_Name","Job_Number","SUBMIT","START","END","Failed","Exit"} {print $0}' \
 | sed 's/ /\t/g'
